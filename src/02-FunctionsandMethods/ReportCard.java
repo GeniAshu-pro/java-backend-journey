475f538d0a908a901216899cc5f848fa4373e666
GeniAshu-pro <pandeyashutosh67215@gmail.com>
2026-08-07 14:00:19 +0530

    Method example:Reportcard

--- src/02-FunctionsandMethods/ReportCard.java
+++ src/02-FunctionsandMethods/ReportCard.java
@@ -7,15 +7,25 @@ public class ReportCard {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter ur mark u got Out of 100");
         int marks = sc.nextInt();
-        tellMeGrade(marks);
-
+        System.out.println("Here is ur Grade!!! " + displayGrade(marks));
+        sc.close();
     }
 
 
+    static String displayGrade(int marks) {
+        if (marks < 0 || marks > 100) {
+            return "Invalid Marks";
+        }
+        if (marks >= 91) return "AA";
+        if (marks >= 81) return "AB";
+        if (marks >= 71) return "BB";
+        if (marks >= 61) return "BC";
+        if (marks >= 51) return "CD";
 
+        return "Fail";
 
-    static int tellMeGrade(int Marks ) {
 
     }
-    }
+}
+
 

664588aedb2735432ff48efc1fd9c281c3ec61f6
GeniAshu-pro <pandeyashutosh67215@gmail.com>
2026-08-13 07:47:46 +0530

    "Add Collection framework tools : Set"

--- src/_05_Collections/_03_Set/SetDemo.java
+++ src/_05_Collections/_03_Set/SetDemo.java
@@ -1,4 +1,19 @@
 package _05_Collections._03_Set;
 
-public class SetDemo {
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetDemo  {
+    public static void main(String[] args) {
+        Set<String> uses =new HashSet<>();
+        uses.add("Ashutosh");
+        uses.add("Ashutosh"); // doesn't Contain Duplicates
+        uses.add("Harsh");
+        for(String user:uses){
+            System.out.println(uses);
+        }
+
+
+
+    }
 }

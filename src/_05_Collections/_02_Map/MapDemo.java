05e46dcd0debfb59e49e4c2547238feb8fba61df
GeniAshu-pro <pandeyashutosh67215@gmail.com>
2026-08-13 07:48:10 +0530

    "Add Collection framework tools : Map"

--- src/_05_Collections/_02_Map/MapDemo.java
+++ src/_05_Collections/_02_Map/MapDemo.java
@@ -1,4 +1,20 @@
 package _05_Collections._02_Map;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MapDemo {
+    public static void main(String[] args) {
+        Map<Integer , String> userMap=new HashMap<>();
+        userMap.put(1,"Ashutosh");
+        userMap.put(2,"Satish");
+        userMap.put(3,"maan");
+        userMap.put(4,"Om");
+        userMap.remove(4);
+        System.out.println(userMap.get(3));
+        for (Map.Entry<Integer ,String>entry:userMap.entrySet()){
+            System.out.println(entry.getKey() +": "+entry.getValue());
+        }
+
+    }
 }

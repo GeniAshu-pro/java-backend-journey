4f7177bec595371ed00e1cf7830ef94745f48377
GeniAshu-pro <pandeyashutosh67215@gmail.com>
2026-08-14 15:33:38 +0530

    "Add Exception Handling"

--- src/_06_ExceptionHandling/ExceptionThrowsDemo.java
+++ src/_06_ExceptionHandling/ExceptionThrowsDemo.java
@@ -1,12 +1,19 @@
 package _06_ExceptionHandling;
 
+class UnavailableBookException extends Exception {
+    public UnavailableBookException(String message) {
+        super(message);
+    }
+}
+
 class Library {
     int availableBook = 3;
 
     public void borrowBook(int bookRequest) throws Exception {
         if (bookRequest > availableBook)
-            throw new Exception("Not enough book is available");
-
+            throw new UnavailableBookException("Not Enough Book");
+        if (bookRequest < 0)
+            throw new Exception("At leat request 1 book");
     }
 }
 
@@ -14,9 +21,15 @@ public class ExceptionThrowsDemo {
     public static void main(String[] args) {
         Library library = new Library();
         try {
-            library.borrowBook(1);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+            library.borrowBook(10);
+        } catch (UnavailableBookException e) {
+            e.printStackTrace();
+        }catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Error Occurred :" +e.getMessage());
+        }
+        finally {
+            System.out.println("Finally block ");
         }
 
     }

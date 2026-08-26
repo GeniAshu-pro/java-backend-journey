8d94e6ac45d45958016bd7638308c9975ad9b184
GeniAshu-pro <pandeyashutosh67215@gmail.com>
2026-08-26 08:14:16 +0530

    Mini Project for Spring and Starts of Spring

---
+++ MiniProject-Spring/ProjectSpring/src/main/java/com/example/Controller/UserController.java
@@ -0,0 +1,25 @@
+package com.example.Controller;
+
+import com.example.Service.UserService;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Controller
+public class UserController {
+    private UserService userService ;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    public void createUser(String name){
+     userService.addUser(name);
+        System.out.println("User added: "+name);
+    }
+
+    public void listUser(){
+        List<String> users=userService.getAllUser();
+        System.out.println("All Users: "+users);
+    }
+}

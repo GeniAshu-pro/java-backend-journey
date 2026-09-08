e227dc0b94e1f367f54d1871890d749325c8e9ea
GeniAshu-pro <pandeyashutosh67215@gmail.com>
2026-09-08 12:37:15 +0530

    Function Interface Example

---
+++ src/CoreJAVA/_07_FunctionalInterface/functionInterface/Main.java
@@ -0,0 +1,17 @@
+package _07_FunctionalInterface.functionInterface;
+
+//import static _07_FunctionalInterface.functionInterface.function.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+//          System.out.println(addFunction.apply(10));
+//            System.out.println(subFunction.apply(10));
+//            System.out.println(combinedFunction.apply(10));
+
+        System.out.println(function.addFunction.apply(10));
+        System.out.println(function.subFunction.apply(10));
+        System.out.println(function.combinedFunction.apply(10));
+        }
+    }
+
---
+++ src/CoreJAVA/_07_FunctionalInterface/functionInterface/function.java
@@ -0,0 +1,16 @@
+package _07_FunctionalInterface.functionInterface;
+
+import java.util.function.Function;
+
+public class function {
+    public static Function<Integer , Integer>addFunction = (a)->a+1;
+    public static Function<Integer , Integer>subFunction = (a)->a-1;
+    public static Function<Integer , Integer>combinedFunction = addFunction .andThen(subFunction);
+
+
+//    public static void main(String[] args) {
+//        System.out.println(addFunction.apply(10));
+//        System.out.println(subFunction.apply(10));
+//        System.out.println(combinedFunction.apply(10));
+//    }
+}
